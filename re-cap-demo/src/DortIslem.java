e64ffae95e85af4b8cd80053b25e5dc5a233dd73
Gulsahyldrm <yildirimgulsah2323@gmail.com>
2024-09-01 01:11:00 +0300

    re-cap-demo

---
+++ re-cap-demo/src/DortIslem.java
@@ -0,0 +1,16 @@
+public class DortIslem {
+    public int Summation(int sayi1, int sayi2){
+        return sayi1+sayi2;
+
+    }
+    public int Extraction(int sayi1, int sayi2){
+        return sayi1- sayi2;
+    }
+    public int Multiplication(int sayi1, int sayi2){
+        return sayi1*=sayi2;
+    }
+    public int Division(int sayi1, int sayi2){
+        return sayi1/=sayi2;
+    }
+
+}
---
+++ re-cap-demo/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.Multiplication(9,8);
+        int sonuc2 = dortIslem.Summation(7,1);
+        int sonuc3 = dortIslem.Division(90,2);
+        int sonuc4 = dortIslem.Extraction(23,3);
+        System.out.println(sonuc);
+        System.out.println(sonuc2);
+        System.out.println(sonuc3);
+        System.out.println(sonuc4);
+    }
+}
