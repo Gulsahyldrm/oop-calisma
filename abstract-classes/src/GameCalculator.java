58e88db98a8fbdb6e71ed895ea692f325cf0f31c
Gulsahyldrm <yildirimgulsah2323@gmail.com>
2024-09-04 13:09:08 +0300

    OOP exercise abstact class devamı

--- abstract-classes/src/GameCalculator.java
+++ abstract-classes/src/GameCalculator.java
@@ -1,5 +1,9 @@
-public class GameCalculator {
-    public void hesapla() {
-        System.out.println("Puanınız 100");
+import javax.sound.midi.Soundbank;
+
+public abstract class GameCalculator {
+    public abstract void hesapla() ;
+
+    public final void gameOver(){
+        System.out.println("Oyun Bitti");
     }
 }
--- abstract-classes/src/Main.java
+++ abstract-classes/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        WomanGameCalculator womanGameCalculator = new WomanGameCalculator();
+        womanGameCalculator.hesapla();
+        womanGameCalculator.gameOver();
+
+        //Abstract Classlar tek başına new'lenemez tekrardan methodu Override etmen gerekir ki
+        // o da tercih edilen bir yöntem değildir!
+        // ama inherit ettiğin Classı new leyebilirisin
+
+        GameCalculator gameCalculator = new WomanGameCalculator();
+        GameCalculator gameCalculatorForMan = new ManGameCalculator();
+        GameCalculator gameCalculatorForKids = new KidsGameCalculator();
+
+
+
+
     }
 }
