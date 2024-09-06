dbb6931b10a1e565058c276f38273a852e96ba07
Gulsahyldrm <yildirimgulsah2323@gmail.com>
2024-09-06 23:32:15 +0300

    OOP exercise interfaces

---
+++ interfaces/src/CustomerManager.java
@@ -0,0 +1,9 @@
+public class CustomerManager {
+    public void add(){
+        // iş kodları yazılır bu katmana
+
+        // SAKIN AŞAĞIDAKİ GİBİ BAĞIMLI BİR YAPI KURMA!
+        //OracleCustomerDal oracleCustomerDal = new OracleCustomerDal();
+
+    }
+}
